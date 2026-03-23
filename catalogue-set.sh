00b12097aaf90bbf6c6908ecb3f61346257dff92
Narii Chowdary <nareshchowdary164@gmail.com>
2026-03-23 23:17:08 +0530

    project

--- catalogue-set.sh
+++ catalogue-set.sh
@@ -53,3 +53,4 @@ echo -e "Catalogue application setup ... $G SUCCESS $N"
 
 cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-mongoshsfds -y &>>$LOG_FILE
+INDEX=$(mongosh mongodb.daws86s.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
