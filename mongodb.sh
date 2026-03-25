6179f9734ba854b02add09a7d60a6131d601cf9f
Narii Chowdary <nareshchowdary164@gmail.com>
2026-03-25 22:18:15 +0530

    project

--- mongodb.sh
+++ mongodb.sh
@@ -41,3 +41,7 @@ VALIDATE $? "Start MongoDB"
 
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 VALIDATE $? "Allowing remote connections to MongoDB"
+
+
+systemctl restart mongod
+VALIDATE $? "Restarted MongoDB"
