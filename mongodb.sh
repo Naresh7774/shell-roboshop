a81aab34e3c4c489ea8e1ee183d5dad1f138c4a4
Narii Chowdary <nareshchowdary164@gmail.com>
2026-03-25 22:17:03 +0530

    project

--- mongodb.sh
+++ mongodb.sh
@@ -5,3 +5,7 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
