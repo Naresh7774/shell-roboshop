87919aac0e9bdb6dbef39803d40f688b3cd6f369
Narii Chowdary <nareshchowdary164@gmail.com>
2026-03-25 22:10:25 +0530

    project

--- frontend.sh
+++ frontend.sh
@@ -11,3 +11,4 @@ SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 SCRIPT_DIR=$PWD
 MONGODB_HOST=mongodb.naresh.icu
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
+mkdir -p $LOGS_FOLDER
