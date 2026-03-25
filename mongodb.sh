f7696241d180d9a326d300827134e54eceb18b36
Narii Chowdary <nareshchowdary164@gmail.com>
2026-03-25 22:17:15 +0530

    project

--- mongodb.sh
+++ mongodb.sh
@@ -9,3 +9,6 @@ N="\e[0m"
 LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
