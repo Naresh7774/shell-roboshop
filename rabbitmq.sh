b2b2a929556fce041e82a0c7d7f08fe85962ca23
Narii Chowdary <nareshchowdary164@gmail.com>
2026-03-30 23:04:49 +0530

    project

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -46,3 +46,7 @@ VALIDATE $? "Starting RabbitMQ"
 
 
 VALIDATE $? "Setting up permissions"
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+echo -e "Script executed in: $Y $TOTAL_TIME Seconds $N"
