684b6480295db2ddcfc60bd1445092a29bc9d53d
Narii Chowdary <nareshchowdary164@gmail.com>
2026-03-31 20:08:16 +0530

    project

--- shipping.sh
+++ shipping.sh
@@ -6,7 +6,11 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+
+
 LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 SCRIPT_DIR=$PWD
 MONGODB_HOST=mongodb.daws86s.fun
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
+MYSQL_HOST=mysql.daws86s.fun
